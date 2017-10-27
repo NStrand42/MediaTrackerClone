a61605362750582edc26762fffcf630bc4c57a4e
Nathan Strand <nathan.strand4267@gmail.com>
2017-10-26 18:24:23 -0700

    Initial commit
    Putting initial files in

---
+++ Week5Class/app/src/main/java/it405/college/greenriver/week5class/MainActivity.java
@@ -0,0 +1,13 @@
+package it405.college.greenriver.week5class;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
